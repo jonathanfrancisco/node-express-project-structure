51c2f948fc2befc59148a59726f19f410a833086
Jonathan Francisco <jonathanfrancisco999@gmail.com>
2020-01-27 00:13:15 +0800

    update

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 const express = require('express')
-// const logger = require('morgan')
 const helmet = require('helmet')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const compression = require('compression')
-const winston = require('winston')
 const httpErrors = require('http-errors')
 
 const config = require('../config')
@@ -12,18 +10,8 @@ const expressErrorHandler = require('./expressErrorHandler')
 const apiRoutes = require('./components')
 
 const app = express()
-const logger = winston.createLogger({
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
-      filename: '.logos/test.log'
-    })
-  ]
-})
 
 const setupAndStartExpress = async () => {
-  // app.use(logger('dev'))
-  logger.log('info', 'hello world')
   app.use(helmet())
   app.use(cors())
   app.use(bodyParser.json())
